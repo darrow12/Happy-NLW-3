ac8ae207192679d2ac17d24dbba06687ea496f3d
Darrow <47289706+Darrooooow@users.noreply.github.com>
2020-10-16 17:58:33 -0300

    console.log('Oi');

--- src/server.js
+++ src/server.js
@@ -1,2 +1,9 @@
 // NPM = Node Packge Manager
 // Algo mais ou menos como Gerenciador de pacotes Node
+
+const express = require('express');
+const server = express()
+
+server.get('/', () => {
+    console.log('Oi')
+})
