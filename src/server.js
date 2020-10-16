735801ae3934ceabe6333c451bf32aa273285b7c
Darrow <47289706+Darrooooow@users.noreply.github.com>
2020-10-16 18:02:48 -0300

    Servidor criado!
    Agora é possível acessar o site sem a ajuda do plugin do live server.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 // NPM = Node Packge Manager
 // Algo mais ou menos como Gerenciador de pacotes Node
 
+
+// Importar dependência
 const express = require('express');
+
+ // Iniciando o express
 const server = express()
 
+// Criar uma rota
 server.get('/', () => {
     console.log('Oi')
 })
+
+// Ligar o servidor
+server.listen(5500)
