6784891262cfe33f32fae37fc10370d6c87c2127
Darrow <47289706+Darrooooow@users.noreply.github.com>
2020-10-17 01:00:34 -0300

    Atualização

--- src/server.js
+++ src/server.js
@@ -12,10 +12,16 @@ const server = express()
 server
 // Utilizando os arquivos estáticos
 .use(express.static('public')) // Express cria todas as rotas que precisa no public
+
+    // Configurar template engine
+    .set('views', path.join(__dirname, "views"))
+    .set('view engine', 'hbs')
+
 // Criar uma rota
 .get('/', (request, response) => {
     console.log(request.query)
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+    const name = "Darrow"
+    return response.render('index', {name}) // Quando utiliza o mesmo nome, não é necessário fazer name: <nome_da_variavel> porque ele já entende que o valor está embutido ali
 })
 
 
